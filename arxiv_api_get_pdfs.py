7128b8b41010d5312fb2857c1676e431d4528730
David Swearingen <swearingendw@mac.com>
2020-05-24 10:30:17 -0400

    working

--- arxiv_api_get_pdfs.py
+++ arxiv_api_get_pdfs.py
@@ -18,14 +18,18 @@ SUBJECT = "cs.CR"   # Cyber and Crypto Subject is cs.CR
 url = f'http://export.arxiv.org/api/query?search_query=all:{SUBJECT}&start=0&max_results={MAX_RESULTS}'
 
 # Accepts an XML ElementTree (I think) and searches for all the pdf's, returning
-# a list of URL's to each PDF
+# a list of dictionaries containing metadata about each pdf 
 def get_pdf_urls_from_root(root):
     out = []
     entries = root.findall('ns:entry', namespace)
     for entry in entries:
+        d = dict()
         link = entry.find("ns:link/[@title='pdf']", namespace)
         if link is not None:
-            out.append(link.attrib.get('href'))
+            d['url'] = link.attrib.get('href')
+            d['title'] = entry.find("ns:title", namespace).text
+            d['published'] = entry.find("ns:published", namespace).text
+            out.append(d)
     return out
 
 # Main
@@ -34,13 +38,16 @@ if __name__ == "__main__":
     root = ET.fromstring(requests.get(url).text)  # This is the first request to get just a list of documents
     dct = dict()
     with open('all.json', 'w', encoding='utf8') as fout:
-        for idx, url in enumerate(get_pdf_urls_from_root(root)):
-            print(url)
+        for info_dict in get_pdf_urls_from_root(root):
+            url = info_dict['url']
             text = pdf.get_remote_pdf_text(url).strip()  # This is actually GET'ing a PDF and its text
             sentences, words = prep.clean(text)
             dct['timestamp'] = str(datetime.now())
+            dct['title'] = info_dict['title']
+            dct['published'] = info_dict['published']
             dct['url'] = url
             dct['text'] = text
+            
             # print(json.dumps(dct))        
             # Write out a single file in "JSON Lines" format (every line is a json object)
             # which is suitable for BigQuery upload manually from a google storage bucket,
